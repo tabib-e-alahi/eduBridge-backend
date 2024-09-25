46e48b9536b9e0743e8c2fcc74115f4b019f1886
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-25 19:53:53 +0600

    singllle course data with get id api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,6 +61,13 @@ async function run() {
     // ================ here i am getting all courses ===========
     app.get("/allcourses", async (req, res) => {
       const result = await courseCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/allcourses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await courseCollection.findOne(query);
       res.send(result);
     });
 
